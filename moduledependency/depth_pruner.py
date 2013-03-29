5f49f110dbe119aa86c55e222c10bbc5541a7715
Donald Whyte <donaldwhyte0@gmail.com>
2013-03-29 16:54:22 +0000

    Finished implementing DepthPruner class.

---
+++ moduledependency/depth_pruner.py
@@ -0,0 +1,96 @@
+"""Contains functionality for pruning dependencies with a certain
+depth so higher-level views of dependencies can be constructed."""
+
+class DepthPruner:
+
+    def prunePackageName(self, packageName, depth):
+        """Prune package name to a specified depth.
+
+        Arguments:
+        packageName -- Full name of the package, from the
+                       project root, as a string
+        depth -- Depth to prune name to. For example, if
+                 depth = 1 then "a.b.c" will be pruned
+                 to "a.b". Value must be an integer
+                 greater than or equal to 0.
+
+        """
+        if depth < 0:
+            raise ValueError("Cannot have negative depth")
+        # Split name into its components
+        components = packageName.split(".")
+        # Prune components at the end based on desired depth
+        end = min(depth + 1, len(packageName))
+        return ".".join( components[:end] ) 
+
+    def pruneDependencyList(self, dependantPackageName, dependencies, depth):
+        """Prune name of package and its dependencies.
+
+        A two-element tuple is returned, where the first element is the
+        pruned name of the dependant package and the second element is
+        a set containing the pruned names of any packages/modules that
+        the dependant package dpeends on.
+
+        Any duplicate entries in the dependency list after pruning are
+        removed.
+
+        Arguments:
+        dependantPackageName -- Full name of the package, from the
+                       project root, as a string
+        dependencies -- Collection that contains the full names
+                        names of any packages/modules that the
+                        dependant package depends on
+        depth -- Depth to prune name to. For example, if
+                 depth = 1 then "a.b.c" will be pruned
+                 to "a.b". Value must be an integer
+                 greater than or equal to 0.
+
+
+        """
+        prunedDependantName = self.prunePackageName(dependantPackageName, depth)
+        prunedDependencies = set()
+        for dep in dependencies:
+            prunedDep = self.prunePackageName(dep, depth)
+            prunedDependencies.add(prunedDep)
+        # Ensure recursive dependency isn't present
+        if prunedDependantName in prunedDependencies:
+            prunedDependencies.remove(prunedDependantName)
+        return ( prunedDependantName, prunedDependencies )
+
+    def prune(self, dependencies, depth):
+        """Prune names of a collection of dependant modules and their dependencies.
+
+        Any duplicate entries is the pruned dependency lists are moved.
+        Additionally, the dependencies of any duplicate DEPENDANT package names
+        are merged into a single dependency list.
+
+        Arguments:
+        dependencies -- Returns dictionary where the keys are the packages/modules
+                        in the project and the values are packages/modules that
+                        the respective key imported.
+        depth -- Depth to prune package names to. For example, if depth = 1 then
+                 "a.b.c" will be pruned to "a.b". Value must be an integer
+                 greater than or equal to 0.
+
+        """
+        prunedDependencies = {}
+        for dependantName, dependencyList in dependencies.items():
+            prunedName, prunedSet = self.pruneDependencyList(
+                dependantName, dependencyList, depth)
+            try:
+                # If there's alreayd some dependencies for the pruned dependant
+                # module, merge the current pruned list with the existing list.
+                # Duplicate values are removed using set union
+                existingDeps = prunedDependencies[prunedName]
+                mergedDeps = existingDeps.union( prunedSet )
+                prunedDependencies[prunedName] = mergedDeps
+            # If there are currently no dependencies for the pruned name
+            except KeyError:
+                prunedDependencies[prunedName] = prunedSet
+
+        # TODO
+        for dependantName, dependencySet in prunedDependencies.items():
+            if dependantName in dependencySet:
+                dependencySet.remove(dependantName)
+
+        return prunedDependencies
--- tests/test_depth_pruner.py
+++ tests/test_depth_pruner.py
@@ -15,8 +15,7 @@ class TestDepthPruner(unittest.TestCase):
     def tearDown(self):
         self.pruner = None
 
-    def prunePackageName():
-        # prunePackageDepth("project.pack1.a", 2) = "project.pack1"
+    def test_prunePackageName(self):
         # Test negative value for depth
         with self.assertRaises(ValueError):
             self.pruner.prunePackageName("project.pack1.a", -1)
@@ -35,28 +34,33 @@ class TestDepthPruner(unittest.TestCase):
         # Test five depth where the depth of the name is greater than five
         self.assertEqual( self.pruner.prunePackageName("project.pack1.a.b.c.d.e", 5), "project.pack1.a.b.c.d" )
         
-    def test_pruneDependency():
+    def test_pruneDependency(self):
         # Test with no dependencies
-        self.assertEqual( self.pruner.pruneDependencyList("project.pack1.a", [], 2), [] )
+        self.assertEqual( self.pruner.pruneDependencyList("project.pack1.a", [], 1),
+            ("project.pack1", set()) )
+
+        PRUNED_DEPENDANT_NAME = "project.pack1"
+        PRUNED_DEPENDENCIES = set(["project.z", "project.pack2"])
+
         # Test with many dependencies
-        self.assertEqual( set(self.pruner.pruneDependencyList("project.pack1.a",
-            ["project.z", "project.pack2.a"], 2)),
-            ( "project.pack1", set(["project.z", "project.pack2"]) ) )
+        self.assertEqual( self.pruner.pruneDependencyList(
+            "project.pack1.a", ["project.z", "project.pack2.a"], 1),
+            (PRUNED_DEPENDANT_NAME, PRUNED_DEPENDENCIES) )
         # Test dependencies where two of them will end up identical
         # Ensures that the method removes duplicates that may result
         # from pruning
-        self.assertEqual( set(self.pruner.pruneDependencyList("project.pack1.a",
-            ["project.z", "project.pack2.a", "project.pack2.b"], 2)),
-            ( "project.pack1", set(["project.z", "project.pack2"]) ) )
+        self.assertEqual( self.pruner.pruneDependencyList(
+            "project.pack1.a", ["project.z", "project.pack2.a", "project.pack2.b"], 1),
+              (PRUNED_DEPENDANT_NAME, PRUNED_DEPENDENCIES) )
         # Test dependencies which results in a recursive dependency
         # Ensure that the method removes recursive dependencies form
         # pruned results
-        self.assertEqual( set(self.pruner.pruneDependencyList("project.pack1.a",
-            ["project.z", "project.pack2.a", "project.pack2.b", "project.pack1.b"], 2)),
-            ( "project.pack1", set(["project.z", "project.pack2"]) ) )
+        self.assertEqual( self.pruner.pruneDependencyList(
+            "project.pack1.a", ["project.z", "project.pack2.a", "project.pack2.b", "project.pack1.b"], 1),
+            (PRUNED_DEPENDANT_NAME, PRUNED_DEPENDENCIES) )
 
     def test_prune(self):
-        EXPECTED_DEPENDENCIES = {
+        DEPENDENCIES = {
             "project" : set(["project.pack", "project.pack2"]),
             "project.__main__" : set(["project.a", "project.pack.subpack2"]),
             "project.a" : set(["project.a", "project.pack"]),
@@ -71,24 +75,24 @@ class TestDepthPruner(unittest.TestCase):
             "project.__main__" : set(["project.a", "project.pack"]),
             "project.a" : set(["project.pack"]),
             "project.pack" : set(["project.pack2"]),
-            "project.pack2" : set([])
+            "project.pack2" : set()
         }
         DEPTH2_DEPENDENCIES = {
             "project" : set(["project.pack", "project.pack2"]),
-            "project.__main__" : set(["project.a", "project.pack.subpath2"]),
-            "project.a" : set(["project.a", "project.pack"]),
-            "project.pack.subpath2" : set(["project.pack2.e", "project.pack"]),
+            "project.__main__" : set(["project.a", "project.pack.subpack2"]),
+            "project.a" : set(["project.pack"]),
+            "project.pack.subpack2" : set(["project.pack2.e", "project.pack"]),
             "project.pack2.e" : set(["project.pack2.subpack"]),
-            "project.pack2.subpack" : set(["project2.pack2.e"]),
+            "project.pack2.subpack" : set(["project.pack2.e"]),
         }
 
         # Test with negative value for depth
         with self.assertRaises(ValueError):
-            self.pruner.prune(TEST_DEPENDENCIES, -1)
+            self.pruner.prune(DEPENDENCIES, -1)
         # Test with empty dependencies
         self.assertEqual( self.pruner.prune({}, 0), {})
         # Test with depth 0 (should just be "project => []")
-        self.assertEqual( self.pruner.prune(DEPENDENCIES, 0), {"project" : []})
+        self.assertEqual( self.pruner.prune(DEPENDENCIES, 0), {"project" : set()})
         # Test with depth 1 (this will MERGE many of the dependency lists together)
         self.assertEqual( self.pruner.prune(DEPENDENCIES, 1), DEPTH1_DEPENDENCIES )
         # Test with depth 2
