f1aec5fee37321add3b4ac5194ac7f60086f9de9
Donald Whyte <donaldwhyte0@gmail.com>
2013-07-08 19:27:08 +0100

    Created entry point to moduledependency application

---
+++ moduledependency/__main__.py
@@ -0,0 +1,38 @@
+import sys
+
+from .cli import ArgumentProcessor
+from .executor import Executor
+from .outputter import OutputterFactory
+
+if __name__ == "__main__":
+    # Process command line arguments
+    argProcessor = ArgumentProcessor()
+    try:
+        argProcessor.process(sys.argv)
+    except BaseException as e:
+        sys.exit(str(e))
+
+    outputterFactory = OutputterFactory("moduledependency/outputters")
+    # If an outputter was specified, try and load it
+    if argProcessor.outputterName:
+        # Get all arguments that may be for the outputter (options
+        # not recognised as being anythiing else by the argument processor)
+        outputter = outputterFactory.createOutputter(argProcessor.outputterName, outputterArgs)
+    else:
+        outputter = None
+
+    # Create and configuration main dependency searcher
+    executor = Executor()
+    if outputter:
+        executor.setOutputter(outputter)        
+    try:
+        executor.setMaximumDepth(argProcessor.getOption("depth"))
+    except KeyError:
+        pass
+
+    if not argProcessor.getOption("quiet"):
+        print("starting dependency extraction...")
+    # Search for dependencies in the specified directory
+    dependencies = executor.execute(argProcessor.projectDirectory)
+    if not argProcessor.getOption("quiet"):
+        print("...dependency extraction complete!")
