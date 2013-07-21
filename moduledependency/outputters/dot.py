c4c8a079715dd738d6b7a0adfba9b830a30bed13
Donald Whyte <donaldwhyte0@gmail.com>
2013-07-21 22:03:31 +0100

    Added DOT node ID sanitisation to prevent disallowed characters making the resultant dot script incorrect

--- moduledependency/outputters/dot.py
+++ moduledependency/outputters/dot.py
@@ -1,3 +1,4 @@
+import re
 from moduledependency.outputter import ResultOutputter
 
 # TODO: write docstrings
@@ -10,6 +11,11 @@ class Outputter(ResultOutputter):
     NODE_FORMAT = "\t{};\n"
     EDGE_FORMAT = "\t{} -> {};\n"
 
+    EMPTY_NODE_ID = "__NULL__"
+    DISALLOWED_NODE_CHARACTERS_REGEX = re.compile( r"\W" )
+    DIGIT_REGEX = re.compile( r"\d" )
+    REPLACEMENT_CHARACTER = "_"
+
     def __init__(self, filename=None):
         self.filename = filename
 
@@ -21,11 +27,35 @@ class Outputter(ResultOutputter):
                 f.write(output)
         return output
 
-    def generateDependency(self, dependant, dependancy):
-        return self.EDGE_FORMAT.format(dependant, dependancy)
+    def sanitiseNodeID(self, nodeID):
+        # If ID is empty, return placeholder
+        if len(nodeID) == 0:
+            return self.EMPTY_NODE_ID
+        # Only replace non-word characters INSIDE surrounding quotes
+        if self.surroundedByQuotes(nodeID):
+            return '"{}"'.format( self.replaceNonWordCharacters(nodeID[1:-1]) )
+        # Replace first character if it's a digit
+        if self.isDigit(nodeID[0]):
+            nodeID = self.REPLACEMENT_CHARACTER + nodeID[1:]
+        return self.replaceNonWordCharacters(nodeID)
+
+    def isDigit(self, character):
+        return self.DIGIT_REGEX.match(character)
+
+    def surroundedByQuotes(self, string):
+        return (len(string) >= 3 and string[0] == '"' and string[-1] == '"')
+
+    def replaceNonWordCharacters(self, string):
+        return self.DISALLOWED_NODE_CHARACTERS_REGEX.sub( self.REPLACEMENT_CHARACTER, string )
+
+    def generateDependency(self, dependant, dependency):
+        startNode = self.sanitiseNodeID(dependant)
+        endNode = self.sanitiseNodeID(dependency)
+        return self.EDGE_FORMAT.format(startNode, endNode)
 
     def generateDependant(self, dependant):
-        return self.NODE_FORMAT.format(dependant)
+        node = self.sanitiseNodeID(dependant)
+        return self.NODE_FORMAT.format(node)
 
     def generateGraph(self, dependencies):
         output = ""
--- tests/test_dot_outputter.py
+++ tests/test_dot_outputter.py
@@ -13,10 +13,31 @@ class TestDotOutputter(unittest.TestCase):
         tester = OutputterTestHarness(Outputter,
             # Empty input
             [
-                ({}, "graph dependencies {\n}"),
-                ( {"moda" :["modb", "modc"], "modb" : [], "modc" : ["modd"] },
-                  'graph dependencies {\n\tmoda;\n\tmodb;\n\tmodc;\n\tmoda -> modb;\n\tmoda -> modc;\n\tmodc -> modd;\n}' ),
+                ({}, "digraph dependencies {\n}"),
+                ( {"mod.a" :["mod.b", "mod.c"], "mod.b" : [], "mod.c" : ["mod.d"] },
+                  'digraph dependencies {\n\tmod_a -> mod_b;\n\tmod_a -> mod_c;\n\tmod_b;\n\tmod_c -> mod_d;\n}' ),
             ],
             self.assertEqual,
             True)
         tester.runTests()
+
+    def test_sanitiseNodeID(self):
+        outputter = Outputter()
+        # Empty input
+        self.assertEqual(Outputter.EMPTY_NODE_ID, outputter.sanitiseNodeID(""))
+        # Node with disallowed characters (e.g. dots and dashes, quotes)
+        # Should be replaced with underscores!
+        self.assertEqual("a_b", outputter.sanitiseNodeID("a.b"))
+        self.assertEqual("a_b", outputter.sanitiseNodeID("a-b"))
+        self.assertEqual("a_b_c", outputter.sanitiseNodeID("a_b%c"))
+        self.assertEqual("a_b_c_", outputter.sanitiseNodeID("a_b\"c\""))
+        self.assertEqual("_xy", outputter.sanitiseNodeID("\'xy"))
+        self.assertEqual("_xy_", outputter.sanitiseNodeID("\'xy\'"))
+        self.assertEqual("_", outputter.sanitiseNodeID("."))
+        # Node starting with a digit
+        self.assertEqual("_n", outputter.sanitiseNodeID("3n"))
+        self.assertEqual("_n_x", outputter.sanitiseNodeID("3n.x"))
+        # Node with surrounded by DOUBLE quotes (allowed)
+        self.assertEqual("__", outputter.sanitiseNodeID("\"\""))
+        self.assertEqual("\"hello\"", outputter.sanitiseNodeID("\"hello\""))
+        self.assertEqual("\"a_b\"", outputter.sanitiseNodeID("\"a.b\""))
